b5181b92c8f87be8b40129a47f7a883a7231a5f6
sikandarkumar kumar <sikandarkumarkumar@sikandarkumars-MacBook-Air.local>
2026-01-04 14:29:26 +0530

    fixed eslint issues 2ndtime

--- src/components/Common/Hero.tsx
+++ src/components/Common/Hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion, useViewportScroll} from 'framer-motion';
+import { motion} from 'framer-motion';
 import HeroText from '../Common/HeroText';
 
 const Hero = () => {
