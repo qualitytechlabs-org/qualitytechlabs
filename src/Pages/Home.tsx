84769cb06854d5586401b987a5a8ff56ccbc218f
sikandarkumar kumar <sikandarkumarkumar@sikandarkumars-MacBook-Air.local>
2026-01-04 20:36:24 +0530

    implemented brand logo

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -15,7 +15,6 @@ const Home: React.FC = () => {
       <OurServiceSection/>
       <BuildWaysComparison/>
       <OurProcess/>
-      <h1>This is just for testing ci/cd</h1>
       <FooterMinimal/>
     </div>
   );
